27f8c4be992b3937c7813f011450a0193e979f04
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2019-10-22 19:01:48 -0300

    added getInitialProps to container

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,14 @@ import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 
 class MovieApp extends App {
+  static async getInitialProps(appContext) {
+    const appProps = await App.getInitialProps(appContext)
+
+    return { ...appProps }
+  }
+
   render() {
-    const { Component } = this.props
+    const { Component, pageProps } = this.props
 
     return (
       <div>
@@ -35,7 +41,7 @@ class MovieApp extends App {
         </Head>
         <Navbar />
         <div className="base-page">
-          <Component />
+          <Component {...pageProps} />
         </div>
         <Footer />
 
