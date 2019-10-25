927d9619e0fc461177d545ce46a0bf7ac0418da6
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2019-10-25 18:37:59 -0300

    detail page of movie

--- components/movieList.js
+++ components/movieList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 class MovieList extends React.Component {
   shorten = (text, maxLength) => {
@@ -17,18 +18,20 @@ class MovieList extends React.Component {
         {movies.map(movie => (
           <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
-              <a href="#">
-                <img
-                  className="card-img-top"
-                  src={movie.image}
-                  alt=""
-                />
-              </a>
+              <Link>
+                <a href={`/movies/${movie.id}`}>
+                  <img className="card-img-top" src={movie.image} alt="" />
+                </a>
+              </Link>
               <div className="card-body">
                 <h4 className="card-title">
-                  <a href="#">{movie.name}</a>
+                  <Link>
+                    <a href={`/movies/${movie.id}`}>{movie.name}</a>
+                  </Link>
                 </h4>
-                <p className="card-text">{this.shorten(movie.description, 100)}</p>
+                <p className="card-text">
+                  {this.shorten(movie.description, 100)}
+                </p>
               </div>
               <div className="card-footer">
                 <small className="text-muted">{movie.rating}</small>
