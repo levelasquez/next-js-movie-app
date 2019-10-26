789b202b2bbb91ccf471a25cf4a53d0bd0ebc859
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2019-10-26 20:04:01 -0300

    base server implementation

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const next = require('next')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.use(bodyParser.json())
+
+  server.get('/api/v1/movies', (req, res) => {
+    return res.json({ message: 'Hello World!' })
+  })
+
+  server.post('/api/v1/movies', (req, res) => {
+    const movie = req.body
+
+    return res.json({ ...movie, createdTime: 'today', author: 'me' })
+  })
+
+  server.patch('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+
+    return res.json({ message: `Updating movie for id: ${id}` })
+  })
+
+  server.delete('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+
+    return res.json({ message: `Deleting movie for id: ${id}` })
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  const PORT = process.env.PORT || 3000
+
+  server.listen(PORT, err => {
+    if (err) throw err
+
+    console.log('> Ready on port ' + PORT)
+  })
+})
